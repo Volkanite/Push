b364eff276930c3019fce307725ebaff2571da5f
Volks- <volks.hb@gmail.com>
2014-10-12 16:48:17 -0400

    removed sld3d9hook.h

--- source/_resources/sl/sld3d9hook.h
+++
@@ -1,6 +0,0 @@
-VOID SlHookD3D9(
-    VOID* IDirect3DDevice9_PresentCallback,
-    VOID* IDirect3DSwapChain9_PresentCallback,
-    VOID* IDirect3DDevice9_ResetCallback,
-    VOID* IDirect3D9_CreateDeviceCallback
-    );
