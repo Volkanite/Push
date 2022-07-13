de059a4d2dc5add01aa17610786d3a2acc6c3070
Hakim Boyles <hak@haktronic.com>
2022-07-13 13:40:32 -0400

    GetProcAddress => LdrGetProcedureAddress

--- source/push[exe]/crt.c
+++ source/push[exe]/crt.c
@@ -25,19 +25,19 @@ VOID InitializeCRT()
 
 	ntdll = Module_GetHandle(L"ntdll.dll");
 
-	_iswspace = (TYPE_iswspace)GetProcAddress(ntdll, "iswspace");
-	ntdll_memcmp = (TYPE_memcmp)GetProcAddress(ntdll, "memcmp");
-	ntdll_memcpy = (TYPE_memcpy)GetProcAddress(ntdll, "memcpy");
-	ntdll_memset = (TYPE_memset)GetProcAddress(ntdll, "memset");
-	ntdll_strcmp = (TYPE_strcmp)GetProcAddress(ntdll, "strcmp");
-	ntdll_strcpy = (TYPE_strcpy)GetProcAddress(ntdll, "strcpy");
-	ntdll_strlen = (TYPE_strlen)GetProcAddress(ntdll, "strlen");
-	ntdll_strncmp = (TYPE_strncmp)GetProcAddress(ntdll, "strncmp");
-	strncpy = (TYPE_strncpy)GetProcAddress(ntdll, "strncpy");
-	swscanf_s = (TYPE_swscanf_s)GetProcAddress(ntdll, "swscanf_s");
-	vswprintf_s = (TYPE_vswprintf_s)GetProcAddress(ntdll, "vswprintf_s");
-	wcsncat = (TYPE_wcsncat)GetProcAddress(ntdll, "wcsncat");
-	wcsnlen = (TYPE_wcsnlen)GetProcAddress(ntdll, "wcsnlen");
-	_wcstol = (TYPE_wcstol)GetProcAddress(ntdll, "wcstol");
-	__wtoi = (TYPE__wtoi)GetProcAddress(ntdll, "_wtoi");
+	_iswspace = (TYPE_iswspace)Module_GetProcedureAddress(ntdll, "iswspace");
+	ntdll_memcmp = (TYPE_memcmp)Module_GetProcedureAddress(ntdll, "memcmp");
+	ntdll_memcpy = (TYPE_memcpy)Module_GetProcedureAddress(ntdll, "memcpy");
+	ntdll_memset = (TYPE_memset)Module_GetProcedureAddress(ntdll, "memset");
+	ntdll_strcmp = (TYPE_strcmp)Module_GetProcedureAddress(ntdll, "strcmp");
+	ntdll_strcpy = (TYPE_strcpy)Module_GetProcedureAddress(ntdll, "strcpy");
+	ntdll_strlen = (TYPE_strlen)Module_GetProcedureAddress(ntdll, "strlen");
+	ntdll_strncmp = (TYPE_strncmp)Module_GetProcedureAddress(ntdll, "strncmp");
+	strncpy = (TYPE_strncpy)Module_GetProcedureAddress(ntdll, "strncpy");
+	swscanf_s = (TYPE_swscanf_s)Module_GetProcedureAddress(ntdll, "swscanf_s");
+	vswprintf_s = (TYPE_vswprintf_s)Module_GetProcedureAddress(ntdll, "vswprintf_s");
+	wcsncat = (TYPE_wcsncat)Module_GetProcedureAddress(ntdll, "wcsncat");
+	wcsnlen = (TYPE_wcsnlen)Module_GetProcedureAddress(ntdll, "wcsnlen");
+	_wcstol = (TYPE_wcstol)Module_GetProcedureAddress(ntdll, "wcstol");
+	__wtoi = (TYPE__wtoi)Module_GetProcedureAddress(ntdll, "_wtoi");
 }
