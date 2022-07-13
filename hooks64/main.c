1c81c62e493391830f5f5527cf90d3c2954bc81a
Hakim Boyles <hak@haktronic.com>
2022-07-13 13:23:13 -0400

    housekeeping

--- hooks64/main.c
+++ hooks64/main.c
@@ -1,40 +1,37 @@
 #include <sl.h>
-
 #include "push.h"
+
 typedef VOID(*TYPE_InstallOverlayHook)();
 TYPE_InstallOverlayHook InstallOverlayHook;
+VOID InitializeCRT();
 
 
-NTSTATUS __stdcall NtDelayExecution(
-	BOOLEAN Alertable,
-	LARGE_INTEGER* DelayInterval
-	);
-NTSleep(UINT32 Milliseconds)
+NTSleep( UINT32 Milliseconds )
 {
-	LARGE_INTEGER interval;
+    LARGE_INTEGER interval;
 
-	interval.QuadPart = Milliseconds * -10000LL;
-	NtDelayExecution(FALSE, &interval);
+    interval.QuadPart = Milliseconds * -10000LL;
+    NtDelayExecution(FALSE, &interval);
 }
 
 
 int __stdcall mainCRTStartup()
 {
-	HANDLE overlayLib = NULL;
-	void* prcAddress = 0;
+    HANDLE overlayLib = NULL;
+    void* prcAddress = 0;
 
-	InitializeCRT();
+    InitializeCRT();
 
-	overlayLib = Module_Load(L"overlay64.dll");
-	prcAddress = Module_GetProcedureAddress(overlayLib, "InstallOverlayHook");
+    overlayLib = Module_Load(L"overlay64.dll");
+    prcAddress = Module_GetProcedureAddress(overlayLib, "InstallOverlayHook");
 
-	if (prcAddress)
-	{
-		InstallOverlayHook = (TYPE_InstallOverlayHook)prcAddress;
-		InstallOverlayHook();
-	}
+    if (prcAddress)
+    {
+        InstallOverlayHook = (TYPE_InstallOverlayHook)prcAddress;
+        InstallOverlayHook();
+    }
 
-	NTSleep(0xFFFFFFFF);
+    NTSleep(0xFFFFFFFF);
 
-	return 0;
+    return 0;
 }
